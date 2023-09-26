65eac0af9dff22d1e71184a010179fd7c12eafda
Nabeel Hussain <115703960+M2H-Nabeel@users.noreply.github.com>
2023-09-26 15:45:25 +0200

    Add project files.

---
+++ Strings Comparison/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    public static int CountDifferentCharacters(string str1, string str2)
+    {
+        // Remove duplicate characters from both strings
+        string uniqueStr1 = new string(str1.Distinct().ToArray());
+        string uniqueStr2 = new string(str2.Distinct().ToArray());
+
+        // Calculate the number of different characters
+        int diffCount = 0;
+
+        foreach (char c in uniqueStr1)
+        {
+            if (!uniqueStr2.Contains(c))
+            {
+                diffCount++;
+            }
+        }
+
+        foreach (char c in uniqueStr2)
+        {
+            if (!uniqueStr1.Contains(c))
+            {
+                diffCount++;
+            }
+        }
+
+        return diffCount;
+    }
+
+    static void Main(string[] args)
+    {
+        string str1 = "abc$%^12";
+        string str2 = "!@#$abc";
+
+        int result = CountDifferentCharacters(str1, str2);
+        Console.WriteLine("Number of different characters: " + result); // Output: 4
+    }
+}
---
+++ Strings Comparison/Tests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class StringDifferenceTests
+{
+    [TestMethod]
+    public void TestCountDifferentCharacters()
+    {
+        // Arrange
+        string str1 = "abcdef";
+        string str2 = "bcdegh";
+
+        // Act
+        int result = Program.CountDifferentCharacters(str1, str2);
+
+        // Assert
+        Assert.AreEqual(4, result);
+    }
+
+    [TestMethod]
+    public void TestCountDifferentCharactersWithEmptyStrings()
+    {
+        // Arrange
+        string str1 = "";
+        string str2 = "";
+
+        // Act
+        int result = Program.CountDifferentCharacters(str1, str2);
+
+        // Assert
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void TestCountDifferentCharactersWithDifferentLengths()
+    {
+        // Arrange
+        string str1 = "abc";
+        string str2 = "abcd";
+
+        // Act
+        int result = Program.CountDifferentCharacters(str1, str2);
+
+        // Assert
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void TestCountDifferentCharactersWithSpecialCharactersAndNumbers()
+    {
+        // Arrange
+        string str1 = "abc$%^12";
+        string str2 = "!@#$abc";
+
+        // Act
+        int result = Program.CountDifferentCharacters(str1, str2);
+
+        // Assert
+        Assert.AreEqual(6, result);
+    }
+    [TestMethod]
+    public void TestCountDifferentCharactersWithDuplicatesCharacters()
+    {
+        // Arrange
+        string str1 = "hello";
+        string str2 = "hhelllo";
+
+        // Act
+        int result = Program.CountDifferentCharacters(str1, str2);
+
+        // Assert
+        Assert.AreEqual(0, result);
+    }
+}
